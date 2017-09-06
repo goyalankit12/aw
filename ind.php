e3eb802214d660d246af81addb8226459960c3a4
goyalankit12 <goyalankit2012@gmail.com>
2017-09-06 08:45:04 +0530

    4

--- ind.php
+++ ind.php
@@ -11,7 +11,11 @@ echo "hirr";
 echo "hitttt";
 echo "hi";*/
 $appName = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$connStr = "host=localhost port=5432 dbname=postgres user=postgres password=Ankit@14";
+//$connStr = "host=localhost port=5432 dbname=postgres user=postgres password=Ankit@14";
+
+
+$connStr = "host=ec2-23-21-197-175.compute-1.amazonaws.com port=5432 dbname=d2bu35u7977nam user=zhnknooxixzmqn password=6772ed3336642783bb0c73e57993b98f4f4994386fed3ae08666a0ed9c91ec2a";
+
 
 //simple check
 $conn = pg_connect($connStr);
