0f9d5b5de1cab4c934aac44a71c1b1dcb937e8b0
goyalankit12 <goyalankit2012@gmail.com>
2017-09-07 08:52:15 -0700

    sfo

--- ind.php
+++ ind.php
@@ -5,11 +5,8 @@ $desc = $_POST["desc"];
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 $appName = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-//$connStr = "host=ec2-23-21-197-175.compute-1.amazonaws.com port=5432 dbname=d2bu35u7977nam user=zhnknooxixzmqn password=6772ed3336642783bb0c73e57993b98f4f4994386fed3ae08666a0ed9c91ec2a";
+$connStr = "host=ec2-23-21-197-175.compute-1.amazonaws.com port=5432 dbname=d2bu35u7977nam user=zhnknooxixzmqn password=6772ed3336642783bb0c73e57993b98f4f4994386fed3ae08666a0ed9c91ec2a";
 
-//$conn = pg_connect($connStr);
-
-$connStr = "host=localhost port=5432 dbname=aw user=postgres password=Ankit@14";
 $conn = pg_connect($connStr);
 
 
