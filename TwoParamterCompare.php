ca6ae661020912e290debb0f7ef96ddf0ab03f96
goyalankit12 <goyalankit2012@gmail.com>
2017-09-30 13:52:20 -0700

    assignment2-updated-new-Thursday

--- TwoParamterCompare.php
+++ TwoParamterCompare.php
@@ -54,9 +54,6 @@ $wordSensation['Post Answer']="Post Answers";
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <title>Testing Pie Chart</title>
-    <!--<script type="text/javascript" src="https://mbostock.github.com/d3/d3.js?2.1.3"></script>
-    <script type="text/javascript" src="https://mbostock.github.com/d3/d3.geom.js?2.1.3"></script>
-    <script type="text/javascript" src="https://mbostock.github.com/d3/d3.layout.js?2.1.3"></script>-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://d3js.org/d3.v3.min.js"></script>
 
@@ -84,39 +81,38 @@ $wordSensation['Post Answer']="Post Answers";
         {"label":val2, "value":value2}];
 
     var vis = d3.select("#TwoParameter")
-        .append("svg:svg")              //create the SVG element inside the <body>
-        .data([data])                   //associate our data with the document
-        .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+        .append("svg:svg")
+        .data([data])
+        .attr("width", w)
         .attr("height", h)
-        .append("svg:g")                //make a group to hold our pie chart
-        .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+        .append("svg:g")
+        .attr("transform", "translate(" + r + "," + r + ")")
 
-    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+    var arc = d3.svg.arc()
         .outerRadius(r);
 
-    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
-        .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+    var pie = d3.layout.pie()
+        .value(function(d) { return d.value; });
 
-    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
-        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
-        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-        .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
-        .attr("class", "slice");    //allow us to style things in the slices (like text)
+    var arcs = vis.selectAll("g.slice")
+        .data(pie)
+        .enter()
+        .append("svg:g")
+        .attr("class", "slice");
 
     arcs.append("path")
-        .attr("fill", function(d, i) { return color(i+3); } ) //set the color for each slice to be chosen from the color function defined above
-        .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+        .attr("fill", function(d, i) { return color(i+3); } )
+        .attr("d", arc);
+    arcs.append("svg:text")
+        .attr("transform", function(d) {
 
-    arcs.append("svg:text")                                     //add a label to each slice
-        .attr("transform", function(d) {                    //set the label's origin to the center of the arc
-            //we have to make sure to set these before calling arc.centroid
             d.innerRadius = 0;
             d.outerRadius = r;
-            return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+            return "translate(" + arc.centroid(d) + ")";
 
         })
-        .attr("text-anchor", "middle")                          //center the text on it's origin
-        .text(function(d, i) { return data[i].label; });        //get the label from our original data array
+        .attr("text-anchor", "middle")
+        .text(function(d, i) { return data[i].label; });        
 
 </script>
 </body>
