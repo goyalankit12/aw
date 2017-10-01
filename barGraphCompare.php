ef183fbef7cfe3e83924d49f8e0039b5ab3c3783
goyalankit12 <goyalankit2012@gmail.com>
2017-09-30 18:20:31 -0700

    updated

--- barGraphCompare.php
+++ barGraphCompare.php
@@ -7,8 +7,7 @@
 
 </style>
 <svg width="660" height="400"></svg>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://d3js.org/d3.v3.min.js"></script>
+<script src="https://d3js.org/d3.v4.min.js"></script>
 <script>
 
     var svgBarCompare = d3.select("svg"),
